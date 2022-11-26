0e6e3dc1f61c1437a69bead148f7028cdda336d9
sw08 <lswja6866@naver.com>
2022-11-26 19:53:42 +0900

    [FEAT] Add restart page

--- router.js
+++ router.js
@@ -2,7 +2,8 @@ const fs = require('fs');
 const path = require('path');
 const MarkdownIt = require('markdown-it');
 const plainText = require('markdown-it-plain-text');
-const tool = require('./tool.js')
+const tool = require('./tool.js');
+const shell = require('shelljs');
 
 const md = new MarkdownIt();
 md.use(plainText);
@@ -58,6 +59,18 @@ module.exports = [
         route: '/',
         router: (req, res) => {
             res.render('main', {posts: db.getPosts(keyword='', page=1, per=2, preview=true), notices: db.getNotices()});
+        }
+    },
+    {
+        route: '/restart',
+        router: (req, res) => {
+            const auth = JSON.parse(fs.readFileSync('auth.json'));
+            if (auth.user === req.query.user && auth.password === req.query.password) {
+                shell.exec('git pull');
+                setTimeout(() => {
+                    process.exit(0)
+                }, 3000);
+            }
         }
     },
     {
